6ff292555aa508f3b5b44f9804eaf728c5ab04f2
Chetachi <maduchetat231@gmail.com>
2022-10-03 16:56:44 -0700

    Task

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - outputs absolut value of integer input
+ * @n: integer to check
+ * Return: n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	{
+		n *= -1;
+		return (n);
+	}
+}
