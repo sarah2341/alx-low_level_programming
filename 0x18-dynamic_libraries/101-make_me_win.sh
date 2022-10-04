c48952a27409355c2d956af753e17f9a5591a73d
Chetachi <maduchetat231@gmail.com>
2022-10-03 18:05:34 -0700

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bashw
-get -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+#!/bin/bash
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
